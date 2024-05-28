c0708e957cf2da206ca22b8431d8ec297477ebb7
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-28 14:38:23 +0700

    set up laravel 11 and learn routing dinamis

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get(uri: '/', action: fn () => view(view: "home"));
+Route::get(uri: '/about', action: fn () => view(view: "about"));
+Route::get(uri: '/contact', action: fn () => view(view: "contact"));
+Route::get(uri: '/gallery', action: fn () => view(view: "gallery"));
+
+Route::get(uri: 'users', action: function(){
+    return [
+        ['id' => 1, "name" => "John", "email" => "jhon@gmail.com"],
+        ['id' => 1, "name" => "broe", "email" => "broe@gmail.com"],
+    ];
+});
+
+
