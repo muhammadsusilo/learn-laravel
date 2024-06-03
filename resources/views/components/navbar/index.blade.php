5c1deb83c61686d51d00454c43a3d7a88e11ec3a
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-06-03 16:49:32 +0700

    learn props

---
+++ resources/views/components/navbar/index.blade.php
@@ -0,0 +1,1 @@
+ <nav class="bg-gray-800">
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get(uri: '/', action: fn () => view(view: "home"));
-Route::get(uri: '/about', action: fn () => view(view: "about"));
-Route::get(uri: '/contact', action: fn () => view(view: "contact"));
-Route::get(uri: '/gallery', action: fn () => view(view: "gallery"));
+Route::get(uri: '/', action: fn() => view(view: 'home'));
+Route::get(uri: '/about', action: fn() => view(view: 'about'));
+Route::get(uri: '/contact', action: fn() => view(view: 'contact'));
+Route::get(uri: '/gallery', action: fn() => view(view: 'gallery'));
 
-Route::get(uri: 'users', action: function(){
-    return [
-        ['id' => 1, "name" => "John", "email" => "jhon@gmail.com"],
-        ['id' => 1, "name" => "broe", "email" => "broe@gmail.com"],
-    ];
-});
-
-
+Route::get(uri: 'users',action: function () {
+        return [
+          ['id' => 1, 'name' => 'John', 'email' => 'jhon@gmail.com'],
+          ['id' => 1, 'name' => 'broe', 'email' => 'broe@gmail.com']
+        ];
+    },
+);
